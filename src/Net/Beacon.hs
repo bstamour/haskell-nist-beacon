fdfbd1eec60df1b9163d3a6dbf303ef9f9fd937b
Bryan St. Amour <bryan@bryanstamour.com>
2015-06-09 18:49:47 -0400

    functions now return full records

--- src/Net/Beacon.hs
+++ src/Net/Beacon.hs
@@ -5,65 +5,98 @@
 
      For more information, see the project homepage:
 
-         https://beacon.nist.gov/home
--}
+         https://beacon.nist.gov/home -}
+
 
 module Net.Beacon
-       ( getLastRecord
+       ( Timestamp
+       , Record()
+       , interval
+       , timeStamp
+       , seedValue
+       , previousOutputValue
+       , signatureValue
+       , outputValue
+       , statusCode
+       , getLastRecord
        , getCurrentRecord
        , getPreviousRecord
        , getNextRecord
        , getStartChainRecord
        ) where
 
+
 import Control.Monad
-import Data.Time.Clock.POSIX
+
 import Text.XML.Light.Input
 import Text.XML.Light.Proc
 import Text.XML.Light.Types
+
 import Network.HTTP (getResponseBody, simpleHTTP, getRequest)
 
 import qualified Data.ByteString.Lazy.Char8 as B
 
-timestamp :: IO Int
-timestamp = fmap round getPOSIXTime
+
+data Record = Record { interval            :: Int
+                     , timeStamp           :: Int
+                     , seedValue           :: B.ByteString
+                     , previousOutputValue :: B.ByteString
+                     , signatureValue      :: B.ByteString
+                     , outputValue         :: B.ByteString
+                     , statusCode          :: Int
+                     } deriving (Show, Eq)
+
+
+type Timestamp = Int
+
 
 getXmlData :: String -> IO B.ByteString
 getXmlData url = do
   body <- getResponseBody <=< simpleHTTP $ getRequest url
   return $ B.pack body
 
-getOutputValue :: B.ByteString -> Maybe String
-getOutputValue stuff = do
-  x <- parseXMLDoc stuff
-  c <- findChild (QName "outputValue" Nothing Nothing) x
-  return $ strContent c
 
-getLastRecord :: IO (Maybe B.ByteString)
+getRecord :: B.ByteString -> Maybe Record
+getRecord stuff = do
+  xml <- parseXMLDoc stuff
+  let fc = findChild' xml
+  Record
+    <$> (read   <$> fc "frequency")
+    <*> (read   <$> fc "timeStamp")
+    <*> (B.pack <$> fc "seedValue")
+    <*> (B.pack <$> fc "previousOutputValue")
+    <*> (B.pack <$> fc "signatureValue")
+    <*> (B.pack <$> fc "outputValue")
+    <*> (read   <$> fc "statusCode")
+  where
+    findChild' xml name = strContent <$> findChild (QName name Nothing Nothing) xml
+
+
+getLastRecord :: IO (Maybe Record)
 getLastRecord = do
   x <- getXmlData "http://beacon.nist.gov/rest/record/last"
-  return $ B.pack <$> getOutputValue x
+  return $ getRecord x
 
-getCurrentRecord :: IO (Maybe B.ByteString)
-getCurrentRecord = do
-  ts <- timestamp
+
+getCurrentRecord :: Timestamp -> IO (Maybe Record)
+getCurrentRecord ts = do
   x <- getXmlData $ "http://beacon.nist.gov/rest/record/" ++ (show ts)
-  return $ B.pack <$> getOutputValue x
+  return $ getRecord x
 
-getPreviousRecord :: IO (Maybe B.ByteString)
-getPreviousRecord = do
-  ts <- timestamp
+
+getPreviousRecord :: Timestamp -> IO (Maybe Record)
+getPreviousRecord ts = do
   x <- getXmlData $ "http://beacon.nist.gov/rest/record/previous/" ++ (show ts)
-  return $ B.pack <$> getOutputValue x
+  return $ getRecord x
 
-getNextRecord :: IO (Maybe B.ByteString)
-getNextRecord = do
-  ts <- timestamp
+
+getNextRecord :: Timestamp -> IO (Maybe Record)
+getNextRecord ts = do
   x <- getXmlData $ "http://beacon.nist.gov/rest/record/next/" ++ (show ts)
-  return $ B.pack <$> getOutputValue x
+  return $ getRecord x
 
-getStartChainRecord :: IO (Maybe B.ByteString)
-getStartChainRecord = do
-  ts <- timestamp
+
+getStartChainRecord :: Timestamp -> IO (Maybe Record)
+getStartChainRecord ts = do
   x <- getXmlData $ "http://beacon.nist.gov/rest/record/start-chain/" ++ (show ts)
-  return $ B.pack <$> getOutputValue x
+  return $ getRecord x
