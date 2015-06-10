c9066bd39190fffaba168ddc05a76d657997a54a
Bryan St. Amour <bryan@bryanstamour.com>
2015-06-10 07:39:03 -0400

    documentation

--- src/Net/Beacon.hs
+++ src/Net/Beacon.hs
@@ -1,11 +1,21 @@
-{- | Module for interacting with the NIST Randomness Beacon (prototype)
-     web API. The functions exported from this module return 512-bit
-     full-entropy ByteStrings that you can use for whatever you want.
-     New values are computed every 60 seconds on the NIST server.
+{-|
+Module      : Beacon
+Description : Module for interacting with the NIST Randomness Beacon.
+Copyright   : (c) Bryan St. Amour, 2015
+License     : BSD
+Maintainer  : bryan@bryanstamour.com
+Stability   : experimental
+Portability : POSIX
 
-     For more information, see the project homepage:
+This moduleis for interacting with the NIST Randomness Beacon (prototype)
+web API. The Randomness Beacon is designed to be a public source of random
+information. It generates and publishes 512-bit blocks of random data collected
+from a quantum mechanical process every 60 seconds.
 
-         https://beacon.nist.gov/home -}
+For more information about the project, see
+
+    https://beacon.nist.gov/home
+-}
 
 
 module Net.Beacon
@@ -37,17 +47,79 @@ import Network.HTTP (getResponseBody, simpleHTTP, getRequest)
 import qualified Data.ByteString.Lazy.Char8 as B
 
 
-data Record = Record { interval            :: Int
-                     , timeStamp           :: Int
-                     , seedValue           :: B.ByteString
-                     , previousOutputValue :: B.ByteString
-                     , signatureValue      :: B.ByteString
-                     , outputValue         :: B.ByteString
-                     , statusCode          :: Int
-                     } deriving (Show, Eq)
+-- | A single record: the random data plus some additional information.
+data Record =
+  Record
+  { -- | The time interval, in seconds, between expected records.
+    interval :: Int
+
+    -- | The time the seed value was generated as the number of seconds since
+    --   January 1, 1970.
+  , timeStamp :: Int
+
+    -- | A seed value represented as a 64 byte (512-bit) hex string value.
+  , seedValue :: B.ByteString
+
+    -- | The SHA-512 hash value for the previous record - 64 byte hex string.
+  , previousOutputValue :: B.ByteString
+
+    -- | A digital signature (RSA) computed over (in order):
+    --   version, frequency, timeStamp, seedValue, previousHashValue,
+    --   errorCode.
+    --
+    --   Note: Except for version, the hash is on the byte representations
+    --   and not the string representations of the data values.
+  , signatureValue :: B.ByteString
+
+    -- | The SHA-512 hash of the signatureValue as a 64 byte hex string.
+  , outputValue :: B.ByteString
+
+    -- | The status code value:
+    --     0 - Chain intact, values all good
+    --     1 - Start of a new chain of values, previous hash value will be all
+    --         zeroes
+    --     2 - Time between values is greater than the frequency, but the
+    --         chain is still intact
+  , statusCode :: Int
+  } deriving (Show, Eq)
 
 
 type Timestamp = Int
+
+
+-- | Last record published.
+getLastRecord :: IO (Maybe Record)
+getLastRecord = do
+  x <- getXmlData "http://beacon.nist.gov/rest/record/last"
+  return $ getRecord x
+
+
+-- | Current record, or closest to the timestamp.
+getCurrentRecord :: Timestamp -> IO (Maybe Record)
+getCurrentRecord ts = do
+  x <- getXmlData $ "http://beacon.nist.gov/rest/record/" ++ (show ts)
+  return $ getRecord x
+
+
+-- | Previous record.
+getPreviousRecord :: Timestamp -> IO (Maybe Record)
+getPreviousRecord ts = do
+  x <- getXmlData $ "http://beacon.nist.gov/rest/record/previous/" ++ (show ts)
+  return $ getRecord x
+
+
+-- | Next record.
+getNextRecord :: Timestamp -> IO (Maybe Record)
+getNextRecord ts = do
+  x <- getXmlData $ "http://beacon.nist.gov/rest/record/next/" ++ (show ts)
+  return $ getRecord x
+
+
+-- | Start chain record.
+getStartChainRecord :: Timestamp -> IO (Maybe Record)
+getStartChainRecord ts = do
+  x <- getXmlData $ "http://beacon.nist.gov/rest/record/start-chain/" ++ (show ts)
+  return $ getRecord x
 
 
 getXmlData :: String -> IO B.ByteString
@@ -70,33 +142,3 @@ getRecord stuff = do
     <*> (read   <$> fc "statusCode")
   where
     findChild' xml name = strContent <$> findChild (QName name Nothing Nothing) xml
-
-
-getLastRecord :: IO (Maybe Record)
-getLastRecord = do
-  x <- getXmlData "http://beacon.nist.gov/rest/record/last"
-  return $ getRecord x
-
-
-getCurrentRecord :: Timestamp -> IO (Maybe Record)
-getCurrentRecord ts = do
-  x <- getXmlData $ "http://beacon.nist.gov/rest/record/" ++ (show ts)
-  return $ getRecord x
-
-
-getPreviousRecord :: Timestamp -> IO (Maybe Record)
-getPreviousRecord ts = do
-  x <- getXmlData $ "http://beacon.nist.gov/rest/record/previous/" ++ (show ts)
-  return $ getRecord x
-
-
-getNextRecord :: Timestamp -> IO (Maybe Record)
-getNextRecord ts = do
-  x <- getXmlData $ "http://beacon.nist.gov/rest/record/next/" ++ (show ts)
-  return $ getRecord x
-
-
-getStartChainRecord :: Timestamp -> IO (Maybe Record)
-getStartChainRecord ts = do
-  x <- getXmlData $ "http://beacon.nist.gov/rest/record/start-chain/" ++ (show ts)
-  return $ getRecord x
